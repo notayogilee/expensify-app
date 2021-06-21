59a25ef2a103faa85ec26e894eae3d4ef41307fa
Lee Castelani <notayogilee@gmail.com>
2021-06-21 16:56:40 -0400

    initial setup of redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import getVisibleExpenses from './selectors/expenses';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.subscribe(() => {
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+
+  console.log(visibleExpenses);
+});
+
+store.dispatch(addExpense({ description: 'water bill', amount: 5000 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 100000 }));
+
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
