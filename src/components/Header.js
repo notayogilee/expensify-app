2e91ced3e81bd8449426b092322c488bb15289ac
Lee Castelani <notayogilee@gmail.com>
2021-06-21 11:36:18 -0400

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink exact to="/" activeClassName="selected">Home</NavLink>
+    <NavLink to="/create" activeClassName="selected">Create Expense</NavLink>
+    <NavLink to="/help" activeClassName="selected">Help</NavLink>
+  </header>
+);
+
+export default Header;
