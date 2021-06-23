f54a19828dfeb2e4aec124caebf9d3ac65582d35
Lee Castelani <notayogilee@gmail.com>
2021-06-23 11:42:22 -0400

    add edit expense functionality

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { removeExpense } from '../actions/expenses';
 
 const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
   <div>
-    <h1>{description}</h1>
+    <Link to={`/edit/${id}`} >
+      <h1>{description}</h1>
+    </Link>
     <h2>$ {amount / 100}</h2>
     <h2>This was created on: {createdAt}</h2>
-    <button onClick={() => {
-      dispatch(removeExpense({ id }));
-    }}>Remove</button>
+
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
