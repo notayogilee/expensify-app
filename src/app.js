743529c23ecdbc6d0f100484ed6c9f0919d5cce7
Lee Castelani <notayogilee@gmail.com>
2021-06-23 12:17:28 -0400

    add filtering with date range

--- src/app.js
+++ src/app.js
@@ -3,20 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import getVisibleExpenses from './selectors/expenses';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-
-// store.subscribe(() => {
-//   const state = store.getState();
-//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-
-//   console.log(visibleExpenses);
-// });
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
 store.dispatch(addExpense({ description: 'Gas bill', amount: 1000 }));
