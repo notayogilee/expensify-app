0464d5444cf9108e2f2087caf8a926ae47a0197a
Lee Castelani <notayogilee@gmail.com>
2021-06-25 16:22:26 -0400

    add enzyme test for components: NotFound, ExpenseDashboard, ExpenseList, ExpenseListItem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`} >
       <h1>{description}</h1>
     </Link>
     <h2>$ {amount / 100}</h2>
     <h2>This was created on: {createdAt}</h2>
-
   </div>
 );
 
